fa041b79bcc5c34549f4634b9ff13d9fd25b1e77
Gowtham Behara <gothsiddu@gmail.com>
2019-03-11 15:15:33 +0530

    Create MessageServiceInjector to service dependency injection

---
+++ src/main/java/com/yodlee/dependencyinjection/serviceinjector/MessageServiceInjector.java
@@ -0,0 +1,7 @@
+package com.yodlee.dependencyinjection.serviceinjector;
+
+import com.yodlee.dependencyinjection.consumer.Consumer;
+
+public interface MessageServiceInjector {
+    public Consumer getConsumer();
+}
